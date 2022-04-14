6817e16817dadff3056b069e81e582cb31eddece
syzkii <rifkiardianakbar.raa@gmail.com>
2022-04-15 00:14:50 +0700

    update

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+
+@section('container')
+    <h1>Halaman About</h1>
+    <h3>{{ $name }}</h3>
+    <h5>{{ $email }}</h5>
+    <img src="img/{{ $image }}" alt="kiki" width="200">
+@endsection
---
+++ resources/views/blog.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+
+@section('container')
+    <h2>Blog</h2>
+@endsection
